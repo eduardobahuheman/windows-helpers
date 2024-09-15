8040ac3deeab3539e65ff18134dd4a314cec2342
eduardobahuheman <edu03.bahuheman@gmail.com>
2024-09-15 19:01:02 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from flask import Flask, render_template, request
 
 import random
@@ -6,12 +8,12 @@ app = Flask(__name__)
 
 
 # Simulação de uma IA básica para detectar problemas
-def diagnose_problem(description):
+def diagnose_problem_windows(description):
     # IA pode usar um modelo real ou API GPT
-    common_problems = {
+    common_problems: dict[str, str] = {
         "lento": "O sistema está lento, tente desativar programas em segundo plano e verificar vírus,para isso podemos Desativar programas iniciados com o Windows: Pressione Ctrl + Shift + Esc > vá em Inicializa > desative programas desnecessários,Verificar por malware: Use o Windows Defender ou outro antivírus,Desinstalar programas indesejados: Vá em Configurações > Aplicativos > Aplicativos e recursos,Ajustar as opções de energia: Configurações > Sistema > Energia e suspensão > Configurações adicionais de energia.",
-        "tela azul": "Tela azul detecada, tente verificar drivers e possíveis erros de hardware.",
-        "crash": "Travamento detectado, verifique as atualizações recentes e a integridade dos arquivos do sistema."
+        "tela azul": "tente verificar drivers e possíveis erros de hardware.",
+        "travando": "verifique as atualizações recentes e a integridade dos arquivos do sistema."
     }
 
     for key in common_problems:
@@ -20,6 +22,18 @@ def diagnose_problem(description):
 
     return "Desculpe, não consegui identificar o problema. Tente mais detalhes."
 
+
+def diagnose_problem_hardware(description):
+    # IA pode usar um modelo real ou API GPT
+    common_problems: dict[str, str] = {
+        "esquenta""esquentando""calor""quente":"se o pc esta muito quente,temos varias posibilidades,como o sistema de resfriamento pode estar obistruido por sujeira ou a pasta termica dos componentes estar velha ,os componentes que podemos encontrar pasta termica sao o processador e a placa de video dedicada"
+    }
+
+    for key in common_problems:
+        if key in description.lower():
+            return common_problems[key]
+
+    return "Desculpe, não consegui identificar o problema. Tente mais detalhes."
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
